d2cc60a5b1d1a1ed84b5a454dde034d9c425fa7a
FlashLizard <1256854232@qq.com>
2023-12-15 21:32:03 +0800

    feat: apply to touge

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -12,4 +12,27 @@
 
 long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7);
 
+typedef struct
+{
+  uint32	sh_name;		/* Section name (string tbl index) */
+  uint32	sh_type;		/* Section type */
+  uint64	sh_flags;		/* Section flags */
+  uint64	sh_addr;		/* Section virtual addr at execution */
+  uint64	sh_offset;		/* Section file offset */
+  uint64	sh_size;		/* Section size in bytes */
+  uint32	sh_link;		/* Link to another section */
+  uint32	sh_info;		/* Additional section information */
+  uint64	sh_addralign;		/* Section alignment */
+  uint64	sh_entsize;		/* Entry size if section holds table */
+} Elf64_Shdr;
+
+typedef struct {
+	uint32	st_name; // 4 B (B for bytes)
+	unsigned char	st_info; // 1 B
+	unsigned char	st_other; // 1 B
+	uint16	st_shndx; // 2 B
+	uint64	st_value; // 8 B
+	uint64	st_size; // 8 B
+} Elf64_Sym; 
+
 #endif
--- user/app_print_backtrace.c
+++ user/app_print_backtrace.c
@@ -6,18 +6,14 @@
 #include "user_lib.h"
 #include "util/types.h"
 
-void f8() { print_backtrace(100); }
-void f7() { 
-  int tmp[1000];
-  f8(); }
-void f6() { 
-  int tmp[1002];f7(); }
+void f8() { print_backtrace(7); }
+void f7() { f8(); }
+void f6() { f7(); }
 void f5() { f6(); }
 void f4() { f5(); }
 void f3() { f4(); }
 void f2() { f3(); }
-void f1() { 
-  int tmp[1090];f2(); }
+void f1() { f2(); }
 
 int main(void) {
   printu("back trace the user app in the following:\n");
